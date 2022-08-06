cc621a7e94da90337350d4612fd8ed577e9b7f04
bruno jaen <brunojaen@outlook.com.br>
2022-08-06 08:56:53 -0300

    Adicionando animações aos cards

--- assets/scripts/card-hover-animation.js
+++ assets/scripts/card-hover-animation.js
@@ -1,6 +1,21 @@
-function addEventListenersToCards() {
-  const cardElements = document.getElementsByClassName('s-card');
-  console.log(cardElements)
+function hardleMouseEnter() {
+  this.classList.add('s-card--hovered'); 
+  document.body.id = `${this.id}-hovered`;
 }
 
-document.addEventListener("DOMContentLoaded", addEventListenersToCards, false)
+function handleMouseLeave() {
+  this.classList.remove('s-card--hovered');
+  document.body.id = ``;
+}
+
+function addEventListenersToCards() {
+  const cardElements = document.getElementsByClassName('s-card');  
+
+  for (let index = 0; index < cardElements.length; index++) {
+    const card = cardElements[index];
+    card.addEventListener('mouseenter', hardleMouseEnter);
+    card.addEventListener('mouseleave', handleMouseLeave);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", addEventListenersToCards, false);
